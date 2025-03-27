556154c0772a18e01cc2b867423a60a1479e2015
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-03-27 23:08:46 +0800

    works now

--- upper_function.py
+++ upper_function.py
@@ -1,10 +1,12 @@
 word = input("Enter any word: ")
 
 def isupper(word_input):
+    yep_upper = False #assumes its false first
     for char in word_input: 
-        if char >= 'A' and char <= 'Z':
-            return True
-        else:
+        if char >= "a" and char <= "z":
             return False
+        if char >= "A" and char <= "Z":
+            yep_upper = True
+    return yep_upper
 
 print(isupper(word))
