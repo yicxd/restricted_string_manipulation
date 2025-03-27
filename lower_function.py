b14904e79aac271f1e3bb638426409b469125fbc
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-03-27 21:56:27 +0800

    its either ord or a very long set

--- lower_function.py
+++ lower_function.py
@@ -1,3 +1,15 @@
 word = input("Enter any word: ")
 
-print(word)
+def lower(word_input):
+    lowercased_word = ""
+    for char in word_input: 
+        if char >= 'A' and char <= 'Z':
+            #each letter has a unicode and the ord function handles it
+            lower_char = chr(ord(char) + 32) #the difference of upper and lowercased characters is 32
+            lowercased_word += lower_char
+        else:
+            lowercased_word += char
+    return lowercased_word
+
+
+print(lower(word))
