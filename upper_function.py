d9744c9cdf83f1b6f19a14cc8fb761bed688bd91
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-03-27 22:32:24 +0800

    works on the first letter only

--- upper_function.py
+++ upper_function.py
@@ -1,3 +1,10 @@
 word = input("Enter any word: ")
 
-print(word)
+def isupper(word_input):
+    for char in word_input: 
+        if char >= 'A' and char <= 'Z':
+            return True
+        else:
+            return False
+
+print(isupper(word))
