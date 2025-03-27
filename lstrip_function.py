e79170950860419b4bb891f00afc165a9c6f4f76
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-03-27 18:00:49 +0800

    created a function

--- lstrip_function.py
+++ lstrip_function.py
@@ -1,2 +1,9 @@
-hi = 2
-print(hi)
+word = []
+
+def lstrip():
+    for char in word:
+        if char.isspace():#if its a space it will remove that character and will continue the loop
+            word.remove(char)
+            continue
+        else: #if its anything else it will break
+            break
