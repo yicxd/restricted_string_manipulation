57b5b48ef027bec9582d77df7799d568a68be286
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-03-28 23:15:15 +0800

    i needed a second word

--- ljust_function.py
+++ ljust_function.py
@@ -1,8 +1,9 @@
-word = input("Enter any word or sentence: ")
+word = input("Enter any word or sentence: ")#can be just spaces
+secword = input("Enter a second word: ")
 
 def ljust(origword, width): #will return the original plus spaces
     return origword + " " * (width - len(origword))#spaces is equal to the width minus the length of the original
 
 firword =   ljust(word, 10)#width can be adjustable
 
-print(firword)
+print(firword, secword)
