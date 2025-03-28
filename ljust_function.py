a88f2186d4b4664bf48e184d2a6e084a49513f06
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-03-28 23:23:58 +0800

    made the width be inputed

--- ljust_function.py
+++ ljust_function.py
@@ -1,9 +1,10 @@
-word = input("Enter any word or sentence: ")#can be just spaces
+word = input("Enter any thing: ")#can be just spaces
 secword = input("Enter a second word: ")
+ljustnum = int(input("Enter a number for adjusting: "))
 
 def ljust(origword, width): #will return the original plus spaces
     return origword + " " * (width - len(origword))#spaces is equal to the width minus the length of the original
 
-firword =   ljust(word, 10)#width can be adjustable
+firword =   ljust(word, ljustnum)
 
 print(firword, secword)
