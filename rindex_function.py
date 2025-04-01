d9a6636ad3afc270f57f2210dc13950383d63eae
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-04-01 21:14:04 +0800

    get the range and for rindex just reverse

---
+++ rindex_function.py
@@ -0,0 +1,16 @@
+initial_index = -1
+
+word = input("Enter a word: ")
+find = input("What word would you like to find: ") 
+
+
+for letters in reversed(range(len(word))):
+    if word[letters : letters + len(find)] == find:
+        initial_index = letters
+        break
+
+
+if initial_index == -1:
+    print("ValueError")
+else:
+    print(initial_index)
